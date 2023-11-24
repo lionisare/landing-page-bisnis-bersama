e1ba478f7446d4c3ed889725622afb9b7fea917c
lionisare <duocode1234567@gmail.com>
2023-11-24 12:49:26 +0700

    update slider testimoni

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ document.addEventListener("click", function (e) {
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// swiper JS image slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
